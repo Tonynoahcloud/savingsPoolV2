80222b6674b46427de1889b73272300111f9feee
tonnero234 <tonynoah1@gmail.com>
2025-11-04 07:34:09 +0100

    refactor my createPlan unit Test

--- test/savingspool.t.sol
+++ test/savingspool.t.sol
@@ -5,8 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {savingsPool} from "../src/savingsPool.sol";
 import {anthonyToken} from "../src/anthonyToken.sol";
 
-
-contract savingsPoolTest is Test{
+contract savingsPoolTest is Test {
     savingsPool public savingspool;
     anthonyToken public token;
     address user = address(1);
@@ -17,33 +16,19 @@ contract savingsPoolTest is Test{
     }
 
     function testcreatePlan_success() public {
-       vm.prank(user);
-       savingspool.createPlan(1000);
-       assertEq(savingspool.userPlanCount(user), 1);
-       (, , , bool withdrawn) = savingspool.userPlans(user, 1);
-       assertFalse(withdrawn, 'no withdrawn yet');
-       (, , uint256 balance, ) = savingspool.userPlans(user, 1);
-       assertEq(balance, 0);
+        vm.prank(user);
+        savingspool.createPlan(1000);
+        assertEq(savingspool.userPlanCount(user), 1);
+        (,,, bool withdrawn) = savingspool.userPlans(user, 1);
+        assertFalse(withdrawn, "no withdrawn yet");
+        (,, uint256 balance,) = savingspool.userPlans(user, 1);
+        assertEq(balance, 0);
     }
 
     function test_Revert_createPlan_whengoalamountiszero() public {
         vm.prank(user);
         vm.expectRevert();
         savingspool.createPlan(0);
-        
-    }
-
-    function test_Revert_createPlan_checkwithdrawiszero() public {
-        vm.prank(user);
-        savingspool.createPlan(1000);
-        (, , , bool withdrawn) = savingspool.userPlans(user, 1);
-        assertFalse(withdrawn, 'no withdrawal yet');
-    }
-   
-    function test_Revert_createPlan_balanceiszero() public {
-        vm.prank(user);
-        (, , uint256 balance, ) = savingspool.userPlans(user, 1);
-        assertEq(balance, 0);
     }
 
     function test_Emit_createPlan() public {
@@ -52,5 +37,4 @@ contract savingsPoolTest is Test{
         emit savingsPool.planCreated(user, 1, 200);
         savingspool.createPlan(200);
     }
-
 }
